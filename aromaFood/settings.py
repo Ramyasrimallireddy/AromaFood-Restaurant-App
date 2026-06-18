ad84f24a08578e3c43788ca88f9fbef8195b79b5
Ramyasrimallireddy <mallireddyramyasri10@gmail.com>
2026-06-18 12:13:40 +0530

    second

--- aromaFood/settings.py
+++ aromaFood/settings.py
@@ -25,7 +25,11 @@ SECRET_KEY = 'django-insecure-j(*)n7d@y7rihhj&w&pvl3-*oxs-0++ydy6*(-+%xf%@*xa-a7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'aromafood-restaurant-app-1.onrender.com',
+    'localhost',
+    '127.0.0.1',
+]
 
 
 # Application definition
